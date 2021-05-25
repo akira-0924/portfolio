adc97e688d74c8333a239755dc81cbb0b4842999
akira_0924 <80752599+akira-0924@users.noreply.github.com>
2021-05-26 00:36:18 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
 const routes = [{
-  path: '/Profile',
+  path: '/',
   name: 'Profile',
   component: Home
 },
