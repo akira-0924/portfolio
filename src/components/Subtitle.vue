616d0996d6c44bb96a0d8adcec8a33e775894524
akira_0924 <80752599+akira-0924@users.noreply.github.com>
2021-06-02 01:04:10 +0900

    Add files via upload

---
+++ src/components/Subtitle.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1 class="pro-title">{{ title }}</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'Subtitle',
+  props: {
+    title: {
+      type: String
+    }
+  }
+}
+</script>
+<style scoped>
+.pro-title{
+  display: inline-block;
+  border-bottom:2px solid black;
+  margin:30px 80px;
+  font-size:40px;
+  padding-top:60px;
+  letter-spacing: 4px;
+}
+</style>
